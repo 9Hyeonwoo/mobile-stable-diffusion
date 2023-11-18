b4c4faf84948de1326c26cb20700df2a1df97f77
9Hyeonwoo <gusdn1397@gmail.com>
2023-11-18 22:27:06 +0900

    add opencl header files

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,29 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+
+#define CL_TARGET_OPENCL_VERSION 200
+#include <CL/opencl.h>
+
+#define CHECK_ERROR(err) \
+    if (err != CL_SUCCESS) {\
+      __android_log_print(ANDROID_LOG_DEBUG, "__TEST__", "BAD %d", err);\
+    }
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_myopencl_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+    cl_platform_id platform;
+    cl_device_id device;
+    cl_int err;
+
+    err = clGetPlatformIDs(1, &platform, nullptr);
+    CHECK_ERROR(err);
+    err = clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
+    CHECK_ERROR(err);
+
     return env->NewStringUTF(hello.c_str());
 }
