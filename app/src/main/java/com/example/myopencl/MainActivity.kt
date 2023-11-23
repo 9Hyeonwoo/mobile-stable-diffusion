e0d1556ffac1986a55bb10e6ec323511ee7899ec
9Hyeonwoo <gusdn1397@gmail.com>
2023-11-23 22:18:23 +0900

    init pytorch lite & inference text_encoder

--- app/src/main/java/com/example/myopencl/MainActivity.kt
+++ app/src/main/java/com/example/myopencl/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.myopencl
 
+import android.content.Context
 import android.content.res.AssetManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.example.myopencl.databinding.ActivityMainBinding
+import org.pytorch.IValue
+import org.pytorch.Module
+import org.pytorch.Tensor
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +24,17 @@ class MainActivity : AppCompatActivity() {
         // Example of a call to a native method
         binding.sampleText.text = "good"
 
-        tokenize("hello world").forEach {
-            Log.d("__TEST__token", it.toString())
-        }
+        val token = tokenize("a professional photograph of an astronaut riding a horse")
+        val tensor = Tensor.fromBlob(token, longArrayOf(1, token.size.toLong()))
+
+        val module = Module.load(assetsFilePath(this, "text_encoder.ptl"))
+
+        // output shape = (batch_size=1, context_length=77, 1024)
+        val output = module.forward(IValue.from(tensor)).toTensor().dataAsFloatArray
+
+        val test1 = readFloatFromAssets(this, "encoder_test1.txt")
+
+        module.destroy()
     }
 
     /**
@@ -38,6 +48,32 @@ class MainActivity : AppCompatActivity() {
         // Used to load the 'myopencl' library on application startup.
         init {
             System.loadLibrary("myopencl")
+        }
+
+        fun assetsFilePath(context: Context, assetName: String): String {
+            val file = context.filesDir.resolve(assetName)
+            if (file.exists() && file.length() > 0) {
+                return file.absolutePath
+            }
+            context.assets.open(assetName).use { inputStream ->
+                file.outputStream().use { outputStream ->
+                    val buffer = ByteArray(4 * 1024)
+                    var read: Int = inputStream.read(buffer)
+                    while (read != -1) {
+                        outputStream.write(buffer, 0, read)
+                        read = inputStream.read(buffer)
+                    }
+                    outputStream.flush()
+                }
+                return file.absolutePath
+            }
+        }
+
+        fun readFloatFromAssets(context: Context, assetName: String): Sequence<Float> {
+            context.assets.open(assetName).use { inputStream ->
+                val regex = Regex("-?\\d+.\\d+(e[-|+]\\d+)?")
+                return regex.findAll(inputStream.reader().readText()).map { it.value.toFloat() }
+            }
         }
     }
 }
