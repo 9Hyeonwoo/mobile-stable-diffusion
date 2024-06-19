be2b429db25b6d283a0b5452043b19c874cecf8d
9Hyeonwoo <gusdn1397@gmail.com>
2024-06-20 03:48:59 +0900

    Reuse Kernel : CrossAttentionKernel 추가

---
+++ app/src/main/cpp/modules/kernel/unit/CrossAttentionKernel.cpp
@@ -0,0 +1,44 @@
+//
+// Created by 구현우 on 2024/06/20.
+//
+
+#include "CrossAttentionKernel.h"
+#include "../../util.h"
+#include <android/log.h>
+
+#define LOG_TAG "CROSS_ATTENTION_KERNEL"
+
+#define CHECK_ERROR_THROW(err) \
+    if (err != CL_SUCCESS) { \
+      __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, "[%s:%d] OpenCL error %d\n", __FILE__, __LINE__, err); \
+      throw std::runtime_error("OpenCL error."); \
+    }                          \
+
+
+CrossAttentionKernel::CrossAttentionKernel(
+        cl_context context,
+        cl_device_id deviceId,
+        AAssetManager *assetManager
+) {
+    cl_int err;
+
+    auto program = util::create_and_build_program_with_source(
+            context,
+            deviceId,
+            assetManager,
+            "kernel/cross_attention.cl"
+    );
+
+    einsum_bik_bjk_bij = clCreateKernel(program, "einsum_bik_bjk_bij", &err);
+    CHECK_ERROR_THROW(err);
+
+    einsum_bij_bjk_bik = clCreateKernel(program, "einsum_bij_bjk_bik", &err);
+    CHECK_ERROR_THROW(err);
+
+    clReleaseProgram(program);
+}
+
+CrossAttentionKernel::~CrossAttentionKernel() {
+    clReleaseKernel(einsum_bik_bjk_bij);
+    clReleaseKernel(einsum_bij_bjk_bik);
+}
---
+++ app/src/main/cpp/modules/kernel/unit/CrossAttentionKernel.h
@@ -0,0 +1,22 @@
+//
+// Created by 구현우 on 2024/06/20.
+//
+
+#ifndef MY_OPENCL_CROSSATTENTIONKERNEL_H
+#define MY_OPENCL_CROSSATTENTIONKERNEL_H
+
+#include <CL/opencl.h>
+#include <android/asset_manager.h>
+
+class CrossAttentionKernel {
+public:
+    CrossAttentionKernel(cl_context context, cl_device_id deviceId, AAssetManager *assetManager);
+
+    ~CrossAttentionKernel();
+
+    cl_kernel einsum_bik_bjk_bij;
+    cl_kernel einsum_bij_bjk_bik;
+};
+
+
+#endif //MY_OPENCL_CROSSATTENTIONKERNEL_H
